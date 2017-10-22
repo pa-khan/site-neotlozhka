9acaa7ad297e4306ebda3ff11d2a83c0e5f5f263
e5ash <e5ash.bro@gmail.com>
2017-10-22 19:45:18 +0300

    Styling (desktop) 2/3

--- app/template/scripts/common.js
+++ app/template/scripts/common.js
@@ -1,5 +1,23 @@
 $(document).ready(function($) {
 
-	
-	
+	$('.slider').slick({
+		dots: true,
+		arrows: false
+	});
+
+	function valueElementForm(nameElement) {
+		var newNameElement = '.' + nameElement;
+		element = $(newNameElement);
+		element.each(function(index, el) {
+			var elementInput = $(this).find($('input')),
+				elementLabel = $(this).find($('label')),
+				elementValue = index + 1;
+			elementInput.attr('id', nameElement + '-' + elementValue);
+			elementLabel.attr('for', nameElement + '-' + elementValue);
+		});
+
+	}
+	valueElementForm('radio');
+	valueElementForm('checkbox');
+
 });
