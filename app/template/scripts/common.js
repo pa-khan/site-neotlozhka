f3cf948e73118b27d1fba35e9009870833bbd009
e5ash <e5ash.bro@gmail.com>
2017-10-24 14:12:12 +0300

    Calc + fixed: link in logo + checkbox in order page

--- app/template/scripts/common.js
+++ app/template/scripts/common.js
@@ -70,4 +70,94 @@ $(document).ready(function($) {
 	})
 
 	$('.input_phone').mask('+7 (999) 999-99-99');
+
+
+	/* Calc */
+	var calcSelect = $('.select-calc'),
+			calcSum = $('.sum-calc'),
+			calcDiscount = $('.discount-calc'),
+			calcTotal = $('.total-calc'),
+			calcCurrency = $('#select-currency');
+
+	calcSum.html(0);
+
+	
+	
+	function sum(count) {
+		var sum = Number(calcSum.html()),
+				count = Number(count),
+				plus = sum + count,
+				plus = String(plus);
+		calcSum.html(plus);
+	}
+	function discount() {
+		var sum = Number(calcSum.html()),
+				discount = Number(calcDiscount.html());
+
+		calcTotal.html(sum - (sum/100*discount));
+	}
+	
+	
+	function splitCount(value) {
+		var newValue = String(value.html());
+		value.html(newValue.replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 '));
+	}
+
+	function currencyIcon(name) {
+		if(name == 'dollar'){
+			return "$";
+		}
+		if(name == 'ruble'){
+			return "р.";
+		}
+		if(name == 'euro'){
+			return "€";
+		}
+		if(name == 'pound'){
+			return "£";
+		}
+	}
+	function currency() {
+		var calcCurrencyVal = calcCurrency.val(),
+				calcCurrencyMultiplier = Number(calcCurrency.find('option[value = '+ calcCurrencyVal +']').attr('data-currency-value'),
+				total = calcTotal.html()),
+				totalCurrency = total * calcCurrencyMultiplier;
+		$('.total-count').each(function () {
+
+			var unit = $(this).attr('data-unit'),
+					value = $(this).html().replace(' ', ''),
+					block = $('.price__currency[data-unit = '+ unit +']'),
+					total = (Number(value) / calcCurrencyMultiplier).toFixed(2);
+
+
+			block.html(currencyIcon(calcCurrencyVal) + " " + total);
+
+		})
+
+	}
+
+
+	function calc() {
+		calcSum.html(0);
+		calcSelect.each(function () {
+			var optionSelected = $(this).find('option:selected'),
+				optionSelectedPrice = optionSelected.attr('data-price-value');
+			sum(optionSelectedPrice);
+			discount();
+		})
+		splitCount(calcSum);
+		splitCount(calcTotal);
+		currency();
+	}
+
+
+	calc();
+	calcSelect.change(function () {
+		calc();
+	})
+
+	calcCurrency.change(function () {
+		currency();
+	})
+
 });
